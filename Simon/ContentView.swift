49291145115d2d69ee6a229c0c7fbf15503e959b
Chase <Chashiguchi@gmail.com>
2024-09-19 09:35:00 -0500

    made title and set color scheme to dark

--- Simon/ContentView.swift
+++ Simon/ContentView.swift
@@ -10,11 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Simon")
+                .font(.system(size: 72))
         }
+        .preferredColorScheme(.dark)
         .padding()
     }
 }
