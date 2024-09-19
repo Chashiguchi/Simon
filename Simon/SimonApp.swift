6d2b11e8fe71defb52180b7edea558fc69e1cb48
Chase <Chashiguchi@gmail.com>
2024-09-19 09:31:32 -0500

    Initial Commit

---
+++ Simon/SimonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimonApp.swift
+//  Simon
+//
+//  Created by Chase Hashiguchi on 9/19/24.
+//
+
+import SwiftUI
+
+@main
+struct SimonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
