3811b09bd8064298803b13452c129a58d7e31a99
Chase <Chashiguchi@gmail.com>
2024-09-19 09:38:19 -0500

    Added flash functionality to color displays

--- Simon/ContentView.swift
+++ Simon/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var colorDisplay = [ColorDisplay(color: .green),ColorDisplay(color: .red),ColorDisplay(color: .yellow),ColorDisplay(color: .blue)]
+    @State private var flash = [false, false, false, false]
     var body: some View {
         VStack {
             Text("Simon")
@@ -16,12 +17,16 @@ struct ContentView: View {
                 .padding()
             HStack {
                 colorDisplay[0]
+                    .opacity(flash[0] ? 1 : 0.4)
                 colorDisplay[1]
+                    .opacity(flash[1] ? 1 : 0.4)
             }
             
             HStack {
                 colorDisplay[2]
+                    .opacity(flash[2] ? 1 : 0.4)
                 colorDisplay[3]
+                    .opacity(flash[3] ? 1 : 0.4)
             }
         }
         .preferredColorScheme(.dark)
