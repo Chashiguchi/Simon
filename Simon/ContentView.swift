476ae3aa4b066642fe40a6342b6492d505fc617f
Chase <Chashiguchi@gmail.com>
2024-09-19 09:49:50 -0500

    added Ability to flash a random sequence of colors

--- Simon/ContentView.swift
+++ Simon/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     @State private var colorDisplay = [ColorDisplay(color: .green),ColorDisplay(color: .red),ColorDisplay(color: .yellow),ColorDisplay(color: .blue)]
     @State private var flash = [false, false, false, false]
+    @State private var timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect()
+    @State private var index = 0
+    @State private var sequence: [Int] = []
     var body: some View {
         VStack {
             Text("Simon")
@@ -42,7 +45,15 @@ struct ContentView: View {
             }
         }
         .preferredColorScheme(.dark)
-        .padding()
+        .onReceive(timer) { _ in
+            if index < sequence.count {
+                flashColorDisplay(index: sequence[index])
+                index += 1
+            } else {
+                index = 0
+                sequence.append(Int.random(in: 0...3))
+            }
+        }
     }
     func flashColorDisplay(index: Int) {
         flash[index].toggle()
